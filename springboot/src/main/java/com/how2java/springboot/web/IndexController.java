d1e331f7e7c7bd1d2839f062f702dd83c1b9303e
dinghandu <thtu@foxmail.com>
2020-05-20 19:36:11 +0800

    refactor/add index

---
+++ springboot/src/main/java/com/how2java/springboot/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.how2java.springboot.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class IndexController {
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+}
